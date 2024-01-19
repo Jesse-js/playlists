7f4d776af7ba1b981f22b52546a4e82063ef1377
Jesse-js <jesse-jjs@hotmail.com>
2024-01-19 13:25:18 -0300

    refactor: final adjusts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
         <div class="row">
             <div class="col-lg-12 margin-tb">
                 <div class="pull-left">
-                    <h2>Welcome</h2>
+                    <h2>Welcome, to the SectoTeca!</h2>
+                    <p>Use the "SectoTeca" menu to explore our playlists and content!</p>
                 </div>
             </div>
         </div>
